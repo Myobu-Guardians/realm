956916cb2ac96cf277cfbce6fdb2013c088f8fab
Kirito M <111298477+kirito-m@users.noreply.github.com>
2022-09-27 21:15:33 +0800

    feat: Initial commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-22.05"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in (rec {
+        devShell = import ./shell.nix { inherit pkgs; };
+      }));
+}
