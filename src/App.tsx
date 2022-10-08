a9fd9defd3bac21af169c24048f25c3b1b4a5f84
Kirito M <111298477+kirito-m@users.noreply.github.com>
2022-10-09 00:58:05 +0800

    fix: Fixed drawer style

--- src/App.tsx
+++ src/App.tsx
@@ -72,44 +72,49 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="drawer drawer-mobile">
-        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content pt-2 px-0 sm:pt-12 sm:px-2">
-          {/** right panel */}
-          <div className="flex flex-col sm:flex-row flex-wrap justify-center sm:justify-start">
-            {feedElements}
+      <div>
+        <div
+          className="drawer drawer-mobile"
+          style={{ height: `calc(100vh - 64px)` }}
+        >
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content pt-2 px-0 sm:pt-12 sm:px-2">
+            {/** right panel */}
+            <div className="flex flex-col sm:flex-row flex-wrap justify-center sm:justify-start">
+              {feedElements}
+            </div>
           </div>
-        </div>
-        <div className="drawer-side z-10">
-          <label htmlFor="my-drawer" className="drawer-overlay"></label>
-          {/* left panel */}
-          <div className="px-0 sm:px-4 pt-2 sm:pt-14 bg-gray-800">
-            {appContainer.signerProfile ? (
-              <div>
-                <MNSProfileCard
-                  labels={["MNS"]}
-                  profile={appContainer.signerProfile}
-                ></MNSProfileCard>
-              </div>
-            ) : (
-              <div className="relative">
-                <div className="blur-sm mx-auto">
+          <div className="drawer-side">
+            <label htmlFor="my-drawer" className="drawer-overlay"></label>
+            {/* left panel */}
+            <div className="px-0 sm:px-4 pt-2 sm:pt-14 bg-gray-800">
+              {appContainer.signerProfile ? (
+                <div>
                   <MNSProfileCard
                     labels={["MNS"]}
-                    profile={{
-                      name: "mns",
-                      displayName: "Myobu Name Service",
-                    }}
+                    profile={appContainer.signerProfile}
                   ></MNSProfileCard>
                 </div>
-                <a
-                  href="https://protocol.myobu.io/#/mns"
-                  className="btn btn-secondary absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
-                >
-                  Register for your account
-                </a>
-              </div>
-            )}
+              ) : (
+                <div className="relative">
+                  <div className="blur-sm mx-auto">
+                    <MNSProfileCard
+                      labels={["MNS"]}
+                      profile={{
+                        name: "mns",
+                        displayName: "Myobu Name Service",
+                      }}
+                    ></MNSProfileCard>
+                  </div>
+                  <a
+                    href="https://protocol.myobu.io/#/mns"
+                    className="btn btn-secondary absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+                  >
+                    Register for your account
+                  </a>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
